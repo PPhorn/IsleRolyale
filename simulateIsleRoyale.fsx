4cb8f65d2ee3086de81858c2a3be725fb3f2654b
PPhorn <pernillehorn@outlook.com>
2018-12-27 12:18:15 +0100

    Cleaned print of plot data in simulation file

--- simulateIsleRoyale.fsx
+++ simulateIsleRoyale.fsx
@@ -4,22 +4,21 @@ open System.IO
 [<EntryPoint >]
 let main args =
   let ticks = if args.Length < 1 then 40 else int(args.[0])
-  let fileName = if args.Length < 1 then "Pop_IsleRoyale" else args.[1]
-  let boardW = if args.Length < 1 then 10 else int(args.[2])
-  let moose = if args.Length < 1 then 15 else int(args.[3])
-  let mooseRep = if args.Length < 1 then 3 else int(args.[4])
-  let wolves = if args.Length < 1 then 4 else int(args.[5])
-  let wolfRep = if args.Length < 1 then 3 else int(args.[6])
-  let wolfHung = if args.Length < 1 then 4 else int(args.[7])
+  let fileName = if args.Length < 2 then "Pop_IsleRoyale" else args.[1]
+  let boardW = if args.Length < 3 then 10 else int(args.[2])
+  let moose = if args.Length < 4 then 15 else int(args.[3])
+  let mooseRep = if args.Length < 5 then 3 else int(args.[4])
+  let wolves = if args.Length < 6 then 4 else int(args.[5])
+  let wolfRep = if args.Length < 7 then 3 else int(args.[6])
+  let wolfHung = if args.Length < 8 then 4 else int(args.[7])
 //  let givenArray = [|ticks; boardW; moose; mooseRep|]
 
-
-  printfn "Arguments given to the function %A" args
-  //printfn "%A" args.[0]
+  if args <> Array.empty then
+    printfn "Arguments given to the function %A" args
+  else
+    printfn "No arguments given. All arguments set to default values"
 
   let isle = animals.environment(boardW, moose, mooseRep, wolves, wolfRep, wolfHung)
-  //let isle = animals.environment(10, 4, 3, 2, 3, 9)
-  //let en fil
 
   let mutable popCount = "T" + "," + "M" + "," + "W" + "\n" + string 0 + "," + string isle.countMoose + "," + string isle.countWolf
 
